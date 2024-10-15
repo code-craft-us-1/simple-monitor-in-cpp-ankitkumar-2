87c4bc80f4cb6955f9912b29f6e2f477e638c040
Ankit Kumar <113016851+ankitkumar-2@users.noreply.github.com>
2024-10-15 10:07:38 +0000

    remove extra spaces

--- monitor.cpp
+++ monitor.cpp
@@ -15,7 +15,7 @@ for (int i = 0; i < 6; i++) {
   cout << "\r* " << flush;
   sleep_for(seconds(1));
   cout << "\r *" << flush;
-  sleep_for(seconds(1)); } 
+  sleep_for(seconds(1)); }
 }
 
 void displayWarningMessage(const char *vital) {
