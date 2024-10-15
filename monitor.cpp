fac2c8761b6229152c9cfd12be520326952ce01c
Ankit Kumar <113016851+ankitkumar-2@users.noreply.github.com>
2024-10-15 07:14:40 +0000

    4CM

--- monitor.cpp
+++ monitor.cpp
@@ -4,19 +4,18 @@
 #include <chrono>
 #include <iostream>
 using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
-const int MAX_TEMP =102;
-const int MIN_TEMP= 95;
+const int MAX_TEMP = 102;
+const int MIN_TEMP = 95;
 const int MAX_PULSE = 100;
 const int MIN_PULSE = 60;
 const int MIN_SP02 = 90;
 
 void displayWarningPrompt() {
-for (int i = 0; i < 6; i++){
+for (int i = 0; i < 6; i++) {
   cout << "\r* " << flush;
   sleep_for(seconds(1));
   cout << "\r *" << flush;
-  sleep_for(seconds(1));
-} 
+  sleep_for(seconds(1)); } 
 }
 
 void displayWarningMessage(const char *vital) {
@@ -24,17 +23,17 @@ void displayWarningMessage(const char *vital) {
   displayWarningPrompt();
 }
 
-bool isVitalOk(const char *vitalType,float reading, float min, float max) {
-if(reading>max || reading < min){
+bool isVitalOk(const char *vitalType, float reading, float min, float max) {
+if(reading > max || reading < min) {
   displayWarningMessage(vitalType);
   return false;
 }
 return true;
 }
 
-int vitalsOk(float temperature, float pulseRate, float spo2) {  
-  bool isTempratureOk = isVitalOk("Temprature", temperature,MIN_TEMP,MAX_TEMP);
-  bool isPulseRateOk = isVitalOk("PulseRate",pulseRate,MIN_PULSE,MAX_PULSE);
-  bool isSpo2Ok = isVitalOk("SPO2",spo2,MIN_SP02,NULL);
+int vitalsOk(float temperature, float pulseRate, float spo2) {
+  bool isTempratureOk = isVitalOk("Temprature", temperature, MIN_TEMP, MAX_TEMP);
+  bool isPulseRateOk = isVitalOk("PulseRate", pulseRate, MIN_PULSE, MAX_PULSE);
+  bool isSpo2Ok = isVitalOk("SPO2", spo2, MIN_SP02, NULL);
   return isTempratureOk && isPulseRateOk && isSpo2Ok;
 }
